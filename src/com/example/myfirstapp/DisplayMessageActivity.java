748eb044425d7820f7f2b28c9d618716f0de3e8e
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2012-10-30 01:13:23 +0200

    Re-prettify DisplayMessageActivity.java.

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.content.Intent;
 
 public class DisplayMessageActivity extends Activity {
-    @Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    // Get the message from the intent
-    Intent intent = getIntent();
-    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		// Get the message from the intent
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-    // Create the text view
-    TextView textView = new TextView(this);
-    textView.setTextSize(40);
-    textView.setText(message);
+		// Create the text view
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
 
-    // Set the text view as the activity layout
-    setContentView(textView);
-}
+		// Set the text view as the activity layout
+		setContentView(textView);
+	}
 }
